3d2c5455d6db11cb1de8249f3be0f20971ab49d4
Sava2901 <133223306+Sava2901@users.noreply.github.com>
2025-03-25 12:58:04 +0200

    Delete cmake-build-debug directory

--- cmake-build-debug/CMakeFiles/Hash_Table_From_Scratch_C.dir/compiler_depend.ts
+++
@@ -1,2 +0,0 @@
-# CMAKE generated file: DO NOT EDIT!
-# Timestamp file for compiler generated dependencies management for Hash_Table_From_Scratch_C.
--- cmake-build-debug/CMakeFiles/Hash_Table_From_Scratch_C.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for Hash_Table_From_Scratch_C.
-# This may be replaced when dependencies are built.
